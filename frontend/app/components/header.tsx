0752bcd828fbe2e7793eea95faf0611f648f17c2
Alirio Mieres <mie21001@byui.edu>
2024-10-08 03:40:26 -0400

    Update header.tsx

--- frontend/app/components/header.tsx
+++ frontend/app/components/header.tsx
@@ -21,7 +21,7 @@ export default function Header() {
       </div>
       <div className="fixed bottom-0 left-0 mb-4 flex h-auto w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:w-auto lg:bg-none lg:mb-0">
         <a
-          href="https://github.com/Alirio-Mieres"
+          href="https://github.com/Alirio-Mieres/Marvel-Chatbot"
           className="flex items-center justify-center font-nunito text-lg font-bold gap-2"
         >
           <span></span>
